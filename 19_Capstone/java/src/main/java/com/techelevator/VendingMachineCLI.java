50dafa23559acbb1623b9078979df41995dfb965
Willis Koa <williskoa@yahoo.com>
2020-02-06 11:34:21 -0500

    Menu exit, item abstract class

--- 19_Capstone/java/src/main/java/com/techelevator/VendingMachineCLI.java
+++ 19_Capstone/java/src/main/java/com/techelevator/VendingMachineCLI.java
@@ -4,10 +4,19 @@ import com.techelevator.view.Menu;
 
 public class VendingMachineCLI {
 
+	//read the file one line at time
+			//determine what type of object
+			// create the corresponding object for that type
+			// load that object into an array or an arrayList
+	
 	private static final String MAIN_MENU_OPTION_DISPLAY_ITEMS = "Display Vending Machine Items";
 	private static final String MAIN_MENU_OPTION_PURCHASE = "Purchase";
-	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE };
-
+	private static final String MAIN_MENU_OPTION_EXIT = "Exit";
+	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE, MAIN_MENU_OPTION_EXIT };
+	private static final String PURCHASE_MENU_OPTION_FEED_MONEY = "Feed Money";
+	private static final String PURCHASE_MENU_OPTION_SELECT_PRODUCT = "Select Product";
+	private static final String PURCHASE_MENU_OPTION_FINISH_TRANSACTION = "Finish Transaction";
+	private static final String[] PURCHASE_MENU_OPTIONS = {PURCHASE_MENU_OPTION_FEED_MONEY, PURCHASE_MENU_OPTION_SELECT_PRODUCT, PURCHASE_MENU_OPTION_FINISH_TRANSACTION};
 	private Menu menu;
 
 	public VendingMachineCLI(Menu menu) {
@@ -22,6 +31,9 @@ public class VendingMachineCLI {
 				// display vending machine items
 			} else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
 				// do purchase
+				choice = (String) menu.getChoiceFromOptions(PURCHASE_MENU_OPTIONS);
+			} else if (choice.equals(MAIN_MENU_OPTION_EXIT)) {
+				choice = (String) menu.getChoiceFromOptions(MAIN_MENU_OPTIONS);
 			}
 		}
 	}
