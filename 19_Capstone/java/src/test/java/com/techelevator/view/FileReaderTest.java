25055a21d357c8ceda3ef01ca2aba165b7546576
Willis Koa <williskoa@yahoo.com>
2020-02-07 15:16:26 -0500

    Added tests to FileReader

--- 19_Capstone/java/src/test/java/com/techelevator/view/FileReaderTest.java
+++ 19_Capstone/java/src/test/java/com/techelevator/view/FileReaderTest.java
@@ -21,7 +21,7 @@ public class FileReaderTest {
 	}
 	
 	@Test
-	public void testFileReaderMethod_with_one_line() throws FileNotFoundException {
+	public void testFileReaderMethod_with_one_gum() throws FileNotFoundException {
 		FileReader test = new FileReader();
 		List<Item> actual = test.fileReader("test_one_line.csv");
 		List<Item> expected = new ArrayList<>();
@@ -30,6 +30,34 @@ public class FileReaderTest {
 		assertEquals(expected.get(0).getName(), actual.get(0).getName());
 	}
 
+	@Test
+	public void testFileReaderMethod_with_one_chip() throws FileNotFoundException {
+		FileReader test = new FileReader();
+		List<Item> actual = test.fileReader("testchip.csv");
+		List<Item> expected = new ArrayList<>();
+		String [] array = {"A1", "Potato Crisps", "3.05", "Chip"};
+		expected.add(new Chip(array));
+		assertEquals(expected.get(0).getName(), actual.get(0).getName());
+	}
 	
+	@Test
+	public void testFileReaderMethod_with_one_candy() throws FileNotFoundException {
+		FileReader test = new FileReader();
+		List<Item> actual = test.fileReader("testcandy.csv");
+		List<Item> expected = new ArrayList<>();
+		String [] array = {"B1", "Moonpie", "1.80", "Candy"};
+		expected.add(new Candy(array));
+		assertEquals(expected.get(0).getName(), actual.get(0).getName());
+	}
+	
+	@Test
+	public void testFileReaderMethod_with_one_drink() throws FileNotFoundException {
+		FileReader test = new FileReader();
+		List<Item> actual = test.fileReader("testdrink.csv");
+		List<Item> expected = new ArrayList<>();
+		String [] array = {"C1", "Cola", "1.25", "Drink"};
+		expected.add(new Drink(array));
+		assertEquals(expected.get(0).getName(), actual.get(0).getName());
+	}
 //	expected.add(new Chip())
 }
